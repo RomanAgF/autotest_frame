e106ed39f4761440a19e78c8ae158d1d258733e9
romanqa <vatavsem@gmail.com>
2023-08-30 09:48:43 +0300

    first iteration update framework

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,30 +1,47 @@
 from abc import ABC, abstractmethod
+from  selenium.common.exceptions import NoSuchElementException
+
 
 class BasePage(ABC):
     ''''Базовый класс страницы'''
 
-    def __init__(self, driver):
+    @abstractmethod # чтобы нельзя было создать экземпляр BasePage
+    def __init__(self, driver, unique_by_locator: tuple, name, url): # проверяем открылась ли страница по локатору
         self.driver = driver
+        self.unique_by_locator = unique_by_locator
+        self.name = name
+        self.url = url
 
-    @abstractmethod
-    def load(self):
-        '''Загрузка страницы'''
-        pass
-    @abstractmethod
-    def set_email(self, email):
-        '''Ввод электронной почты в поле'''
-        pass
+    def open(self):
+        self.driver.get(self.url)
 
-    @abstractmethod
-    def set_password(self, password):
-        '''Ввод пароля'''
-        pass
-    @abstractmethod
-    def confirm(self):
-        '''Нажатие далее'''
-        pass
+    def is_page_opened(self):
+        try:
+            element = self.driver.find_element(*self.unique_by_locator)
+            return element.is_displayed() # реализовать, is_displayed вернет True или False
+        except NoSuchElementException:
+            return False
 
-    @abstractmethod
-    def click_google(self):
-        '''Нажатие вход черезе гугл аккаунт'''
-        pass
+
+    # @abstractmethod
+    # def load(self):
+    #     '''Загрузка страницы'''
+    #     pass
+    # @abstractmethod
+    # def set_email(self, email):
+    #     '''Ввод электронной почты в поле'''
+    #     pass
+    #
+    # @abstractmethod
+    # def set_password(self, password):
+    #     '''Ввод пароля'''
+    #     pass
+    # @abstractmethod
+    # def confirm(self):
+    #     '''Нажатие далее'''
+    #     pass
+    #
+    # @abstractmethod
+    # def click_google(self):
+    #     '''Нажатие вход черезе гугл аккаунт'''
+    #     pass
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,24 +1,27 @@
+from elements.button import Button
+from elements.text_field import TextField
 from .base_page import BasePage
-from elements.main_page_elements import MainPageElements
+from selenium.webdriver.common.by import By
+
 
 class MainPage(BasePage):
     '''Главная страницы'''
+    UNIQUE_LOCATOR = (By.ID, 'identifierNext')
+    AUTH_BUTTON_LOCATOR = (By.ID, 'ddd')
+
     def __init__(self, driver):
-        self.driver = driver
-        self.url = "https://eu.account.battle.net/login/ru/"
-        self.elements = MainPageElements # Получаем нужыне элементы со страницы
+        super().__init__(driver=driver,
+                         unique_by_locator=self.UNIQUE_LOCATOR,
+                         name='Main Page',
+                         url='some_url')
+        self.auth_button = Button(driver=driver, by_locator=self.AUTH_BUTTON_LOCATOR, name='Auth Button')
+        self.search_form = TextField(driver=driver, by_locator=self.SEARCH_FORM_LOCATOR, name='Search Form')
+        self.search_button = TextField(driver=driver, by_locator=self.SEARCH_BUTTON_LOCATOR, name='Search Button')
+    def fill_search_form(self, value):
+        self.search_form.send_text(value)
 
-    def load(self):
-        self.driver.get(self.url)
+    def click_search_button(self):
+        self.search_button.click()
 
-    def click_google(self):
-        self.driver.find_element(*self.elements.GOOGLE_BUTTON).click()
 
-    def set_email(self, email):
-        pass
 
-    def set_password(self, password):
-        pass
-
-    def confirm(self):
-        pass
